6b43e723b62410842a089ca4eaf65f722d4de2a3
Burney, Moe (AU - Sydney) <moburney@deloitte.com.au>
2019-11-26 12:03:25 +1100

    created project

---
+++ weatherapp/weatherapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by Burney, Moe (AU - Sydney) on 26/11/19.
+//  Copyright © 2019 Burney, Moe (AU - Sydney). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
