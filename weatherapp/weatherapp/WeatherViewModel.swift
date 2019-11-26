abfe8d962cdb65e8664ba1412d92ae34c6e77a4a
Burney, Moe (AU - Sydney) <moburney@deloitte.com.au>
2019-11-26 12:26:04 +1100

    view model

---
+++ weatherapp/weatherapp/WeatherViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherViewModel.swift
+//  weatherapp
+//
+//  Created by Burney, Moe (AU - Sydney) on 26/11/19.
+//  Copyright © 2019 Burney, Moe (AU - Sydney). All rights reserved.
+//
+
+import Foundation
